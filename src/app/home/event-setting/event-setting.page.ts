1cc6216323af5005d8d50e96e45c7e01e87a3262
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2021-03-15 15:27:39 +0100

    update event-setting page

--- src/app/home/event-setting/event-setting.page.ts
+++ src/app/home/event-setting/event-setting.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-setting.page.scss'],
 })
 export class EventSettingPage implements OnInit {
+  public items = [
+    {
+      name: 'Patrick Lee',
+      role: 'Velit suspendisse ',
+      img: 'assets/imgs/avatar.png',
+    },
+    {
+      name: 'PAssociation Annie',
+      role: 'Velit suspendisse ',
+      img: 'assets/imgs/avatar.png',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
