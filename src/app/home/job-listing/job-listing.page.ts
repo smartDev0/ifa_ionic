4295c0f7f011e912a60464c316bb3705e075ee1d
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2021-03-16 04:05:17 +0100

    update job-listing page

--- src/app/home/job-listing/job-listing.page.ts
+++ src/app/home/job-listing/job-listing.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-job-listing',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./job-listing.page.scss'],
 })
 export class JobListingPage implements OnInit {
+  public cards = [
+    {
+      id: '1',
+      name: 'Junior/ Entry Level Estimator (Cabinet Making)',
+      img: 'assets/imgs/job-item.png',
+      position: 'Full-Time Position',
+      location: 'Dandenong, VIC',
+    },
+    {
+      id: '2',
+      name: 'Estimator',
+      img: 'assets/imgs/job-item.png',
+      position: 'Full-Time Position',
+      location: 'Dandenong, VIC',
+    },
+    {
+      id: '3',
+      name: 'Cabinet Maker',
+      img: 'assets/imgs/job-item.png',
+      position: 'Part-Time Position',
+      location: 'Beverley, South Australia.',
+    },
+  ];
+  constructor(private route: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  goItem() {
+    this.route.navigate(['/home/job-listing-item/1']);
   }
-
 }
