96d20ad99a5f4174a3a4fa61a8195d219eb78e67
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2021-03-16 10:00:16 +0100

    update business-create and business-setting page

--- src/app/home/business-setting/business-setting.page.ts
+++ src/app/home/business-setting/business-setting.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./business-setting.page.scss'],
 })
 export class BusinessSettingPage implements OnInit {
+  public items = [
+    {
+      name: 'Patrick Lee',
+      role: 'Velit suspendisse ',
+      img: 'assets/imgs/avatar.png',
+    },
+    {
+      name: 'PAssociation Annie',
+      role: 'Velit suspendisse ',
+      img: 'assets/imgs/avatar.png',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
