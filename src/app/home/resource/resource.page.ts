1832e5a095286e07083006781b7dde46f7fa6347
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2021-03-16 03:04:15 +0100

    update resource page

--- src/app/home/resource/resource.page.ts
+++ src/app/home/resource/resource.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-resource',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resource.page.scss'],
 })
 export class ResourcePage implements OnInit {
+  public resources = [
+    {
+      id: '1',
+      name: 'Feature: Finance (April 2020)',
+      img: 'assets/imgs/icons/pdf.png',
+    },
+    {
+      id: '2',
+      name: 'Feature: Finance (April 2020)',
+      img: 'assets/imgs/icons/pdf.png',
+    },
+    {
+      id: '3',
+      name: 'Feature: Finance (April 2020)',
+      img: 'assets/imgs/icons/pdf.png',
+    },
+  ];
+  constructor(private route: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  goItem() {
+    this.route.navigate(['/home/resource-item/1']);
   }
-
 }
